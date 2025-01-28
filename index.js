602a2bb5f4feadd30b3d09753d4252f282214d73
Saket Sourav <saketsourav3@gmail.com>
2025-01-28 22:21:51 +0530

    hemisphere light

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import * as  THREE from 'three';
 
-const width=window.innerWidth;
-const height=window.innerHeight;
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const width = window.innerWidth;
+const height = window.innerHeight;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
 
@@ -14,25 +14,29 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
-const geometry = new THREE.IcosahedronGeometry(1.0, 2);
-const mat=new THREE.MeshBasicMaterial({color:0xccff});
+const geometry = new THREE.IcosahedronGeometry(1.0, 3);
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flateShading: true,
+});
 
-const mesh=new THREE.Mesh(geometry,mat);
+const mesh = new THREE.Mesh(geometry, mat);
 scene.add(mesh);
 
+const hemilight = new THREE.HemisphereLight(0xffffff, 0x000000);
+scene.add(hemilight);
 
-function animate(t=0){
-    console.log(t)
+function animate(t = 0) {
 
     requestAnimationFrame(animate);
-    mesh.scale.setScalar(Math.cos(t*0.001)+1);
-    renderer.render(scene,camera);
+    // mesh.scale.setScalar(Math.cos(t * 0.001) + 1);
+    renderer.render(scene, camera);
 
 
     // mesh.rotation.x=t/2000;
     // mesh.rotation.y=t/1000;
     // renderer.render(scene,camera);
     // requestAnimationFrame(animate);
-  
+
 }
 animate()
