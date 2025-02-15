c42154b4742025f35550d4a1b3819ca05b58d592
Saket Sourav <saketsourav3@gmail.com>
2025-02-15 23:59:31 +0530

    m

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as  THREE from 'three';
-import {orbitControls} from "jsm/controls/OrbitControls.js"
+import {OrbitControls} from "jsm/controls/OrbitControls.js"
 const width = window.innerWidth;
 const height = window.innerHeight;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -14,7 +14,9 @@ const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
-const controls=new orbitControls(camera, renderer.domElement)
+const controls=new OrbitControls(camera, renderer.domElement);
+controls.enableDamping=true;
+controls.dampingFactor=0.03;
 
 const geometry = new THREE.IcosahedronGeometry(1.0, 6);
 const mat = new THREE.MeshStandardMaterial({
@@ -39,10 +41,9 @@ function animate(t = 0) {
 
     requestAnimationFrame(animate);
     console.log('t=', t)
-    // mesh.scale.setScalar(Math.cos(t * 0.001) + 1);
-    mesh.rotation.y = t * 0.0001;
+    // mesh.rotation.y = t * 0.0001;
     renderer.render(scene, camera);
-
+controls.update()
 
     // mesh.rotation.x=t/2000;
     // mesh.rotation.y=t/1000;
