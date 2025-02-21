aa1e226e0cace2f6082add62c80badf4b8c4c6c3
Saket Sourav <saketsourav3@gmail.com>
2025-02-21 23:54:04 +0530

    image of earth dim night map on material

--- index.js
+++ index.js
@@ -19,18 +19,19 @@ new OrbitControls(camera, renderer.domElement);
 // controls.enableDamping = true;
 // controls.dampingFactor = 0.03;
 
-const geometry = new THREE.IcosahedronGeometry(1, 3);
+const geometry = new THREE.IcosahedronGeometry(1, 8);
 const mat = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    flatShading:true,
+    // color: 0xffffff,
+    // flatShading:true,
+    map:new THREE.TextureLoader().load('/assests/textures/earthlights1k.jpg'),
 });
 
 const earth = new THREE.Mesh(geometry, mat);
 scene.add(earth);
 
 
-// const hemilight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
-const hemilight = new THREE.HemisphereLight(pink, green);
+const hemilight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+// const hemilight = new THREE.HemisphereLight(pink, green);
 scene.add(hemilight);
 
 function animate(t = 0) {
