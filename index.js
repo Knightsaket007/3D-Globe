942ce007669aa5c526b43f280aa1fce14550049d
Saket Sourav <saketsourav3@gmail.com>
2025-02-21 23:51:53 +0530

    color testing

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ new OrbitControls(camera, renderer.domElement);
 // controls.enableDamping = true;
 // controls.dampingFactor = 0.03;
 
-const geometry = new THREE.IcosahedronGeometry(1, 8);
+const geometry = new THREE.IcosahedronGeometry(1, 3);
 const mat = new THREE.MeshStandardMaterial({
     color: 0xffffff,
     flatShading:true,
@@ -29,8 +29,8 @@ const earth = new THREE.Mesh(geometry, mat);
 scene.add(earth);
 
 
-const hemilight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
-// const hemilight = new THREE.HemisphereLight(pink, green);
+// const hemilight = new THREE.HemisphereLight(0x0099ff, 0xaa5500);
+const hemilight = new THREE.HemisphereLight(pink, green);
 scene.add(hemilight);
 
 function animate(t = 0) {
